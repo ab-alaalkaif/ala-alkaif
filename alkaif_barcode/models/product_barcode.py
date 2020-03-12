432ff548af42245fa9e5227e41939b4369c4f4af
baa <benjamin.akboka@gmail.com>
2020-03-12 09:33:39 +0400

    task2207526: changed the relation between barcode and product to be on the templateinstead of the variant

--- alkaif_barcode/models/product_barcode.py
+++ alkaif_barcode/models/product_barcode.py
@@ -8,6 +8,7 @@ class ProductBarcode(models.Model):
     _sql_constraints = [('barcode_unique', 'unique(name)', 'Barcode must be unique')]
 
     name = fields.Char(index=True)
-    product_id = fields.Many2one('product.product')
+    product_template_id = fields.Many2one('product.template')
+    product_id = fields.Many2one('product.product', related='product_template_id.product_variant_id')
     unit_price = fields.Float()
     product_uom_id = fields.Many2one('uom.uom')
