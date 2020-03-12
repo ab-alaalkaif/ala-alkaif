4f28b0201d8d63cb8ae5a7faa57201ed82eadba3
baa <benjamin.akboka@gmail.com>
2020-03-12 10:51:22 +0400

    task2207526: fix a wrong display value on product barcode view

--- alkaif_barcode/__manifest__.py
+++ alkaif_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Alkaif Barcode',
-    'version': '1.0.108',
+    'version': '1.0.109',
     'category': 'Sales',
     'sequence': 1,
     'depends': ['base', 'sale', 'stock', 'account', 'product'],
