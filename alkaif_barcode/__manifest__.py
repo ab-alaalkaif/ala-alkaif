e18499f1212b3c1065a3e5b636a25070403b7bfc
baa <benjamin.akboka@gmail.com>
2020-04-05 09:29:43 +0400

    name on barcode is now required

--- alkaif_barcode/__manifest__.py
+++ alkaif_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Alkaif Barcode',
-    'version': '1.0.123',
+    'version': '1.0.124',
     'category': 'Sales',
     'sequence': 1,
     'depends': ['base', 'sale', 'stock', 'account', 'product'],
