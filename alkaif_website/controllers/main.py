c95701e853794283947aa816575a3dff42c7972a
baa <benjamin.akboka@gmail.com>
2020-04-09 14:33:38 +0400

    add sudo so that public user can access stock info

--- alkaif_website/controllers/main.py
+++ alkaif_website/controllers/main.py
@@ -10,10 +10,9 @@ class WebsiteSaleCustom(WebsiteSale):
         domain = super()._get_search_domain(search, category, attrib_values, search_in_description=search_in_description)
         if not request.website.warehouse_id:
             return domain
-        location_id = request.website.warehouse_id.lot_stock_id.id
-        product_ids = request.env['stock.quant'].search_read([('quantity', '>', 0),
-                                                              '|', ('location_id', 'child_of', location_id),
-                                                              ('location_id', '=', location_id)],
-                                                             ['product_id'])
+        location_id = request.website.sudo().warehouse_id.lot_stock_id.id
+        product_ids = request.env['stock.quant'].sudo().search_read([('quantity', '>', 0),
+                                                                     '|', ('location_id', 'child_of', location_id),
+                                                                     ('location_id', '=', location_id)], ['product_id'])
         uids = set(r.get('product_id')[0] for r in product_ids)
         return expression.AND([domain, [('id', 'in', list(uids))]])
