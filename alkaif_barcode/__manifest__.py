32ccc720efb072ea845489172f2c86025e9df1a6
baa <benjamin.akboka@gmail.com>
2020-04-19 14:51:28 +0400

    [BUMP]

--- alkaif_barcode/__manifest__.py
+++ alkaif_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Alkaif Barcode',
-    'version': '1.0.124',
+    'version': '1.0.140',
     'category': 'Sales',
     'sequence': 1,
     'depends': ['base', 'sale', 'stock', 'account', 'product'],
