364293384d4dc2c43e7404d4222503decea8fe52
baa <baa@odoo.com>
2020-05-05 17:36:13 +0400

    Revert "pos.order.line now convert the uom to the base one and change the qty to respect the old uom"
    This reverts commit 461ab7503aeaa9469e7b5da61cc0636c4d2a7103.

--- alkaif_barcode/models/pos_order.py
+++ alkaif_barcode/models/pos_order.py
@@ -1,19 +1,8 @@
 from odoo import api, fields, models, tools, _
 
 
-class POSOrder(models.Model):
-    _inherit = 'pos.order'
-
-
 class POSOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
     barcode_id = fields.Many2one('product.barcode')
     product_uom_id = fields.Many2one('uom.uom', string='Product UoM', related='')
-
-    @api.model
-    def create(self, vals):
-        res = super().create(vals)
-        res.qty = res.product_uom_id._compute_quantity(res.qty, res.product_uom_id, rounding_method='HALF-UP')
-        res.product_uom_id = res.product_id.uom_id
-        return res
